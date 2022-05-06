f9674f00f049dbc93b9b18879a9fbd85dc2f3b03
Pavel Yanovsky <humgate@gmail.com>
2022-05-06 17:10:27 +0300

    Refactor Repository methods to @Query methods

--- src/main/java/com/humga/repository/DaoCrudRepository.java
+++ src/main/java/com/humga/repository/DaoCrudRepository.java
@@ -4,6 +4,7 @@ import com.humga.entity.Person;
 import com.humga.entity.PersonId;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,14 +13,14 @@ import java.util.List;
 public interface DaoCrudRepository extends CrudRepository<Person, PersonId> {
 
     //List<Person> findPersonByCityOfLiving(String city);
-    @Query("select Person from Person p where p.cityOfLiving=:city")
-    List<Person> findPersonByCityOfLiving(String city);
+    @Query("select p from Person p where p.cityOfLiving=:city")
+    List<Person> findPersonByCityOfLiving(@Param("city") String city);
 
-    List<Person> findByPersonIdNameAndPersonIdSurName(String name, String surname);
+    //List<Person> findByPersonIdNameAndPersonIdSurName(String name, String surname);
+    @Query("select p from Person p where p.personId.name=:name and p.personId.surName=:surname")
+    List<Person> findByPersonIdNameAndPersonIdSurName(@Param("name") String name, @Param("surname") String surname);
 
-    List<Person> findByPersonIdAgeLessThanOrderByPersonIdAge(int age);
-
-
-
-
+    //List<Person> findByPersonIdAgeLessThanOrderByPersonIdAge(int age);
+    @Query("select p from Person p where p.personId.age < :age order by p.personId.age asc")
+    List<Person> findByPersonIdAgeLessThanOrderByPersonIdAge(@Param("age") int age);
 }
